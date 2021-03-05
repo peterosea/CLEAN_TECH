04353da9bc68210f133b5f83772d2d48fe7afcc0
Hansanghyeon <999@hyeon.pro>
2021-03-05 10:40:20 +0900

    :sparkles: google font로 변경

--- Theme/functions/fontawesome.php
+++ Theme/functions/fontawesome.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * fontawsome 😎
+ *
+ * @author       Hansanghyeon
+ * @copyright    Hansanghyeon <999@hyeon.pro>
+ **/
+
 function themeslug_enqueue()
 {
   wp_enqueue_script('fontawsome', 'https://kit.fontawesome.com/a99f480b85.js', array(), null, false);
