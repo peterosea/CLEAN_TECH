1aec414323d51505dde2e7fd8390ca3bab3c2612
porcoRosso <jushan1980@gmail.com>
2021-08-25 19:59:49 +0900

    메인슬라이드 변경 작업

---
+++ Theme/templates/page-home_test.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Template Name: 홈페이지 테스트
+ * Template Post Type: page
+ */
+
+// variables
+$theme_url = get_stylesheet_directory();
+$functions_url = get_stylesheet_directory() . '/functions';
+$img_url = get_home_url() . '/wp-content/uploads';
+$zeplin = get_home_url() . '/wp-content/uploads/zeplin';
+get_template_part('template-parts/head/fullpage');
+get_template_part('template-parts/head/slickslide');
+get_header();
+
+// 우측 고정네비게이션
+get_template_part('template-parts/FixedMenu');
+?>
+<main class="homepage">
+  <div id="fullpage">
+    <?php include_once('home/section1-test.php') ?>
+    <?php include_once('home/section2.php') ?>
+    <?php include_once('home/section3.php') ?>
+    <?php include_once('home/section4.php') ?>
+    <?php get_template_part('template-parts/FNB') ?>
+  </div>
+  <ul id="myMenu" class="listStyle">
+    <li data-menuanchor="firstPage" class="active">
+      <div class="label"></div>
+      <a href="#firstPage">메인</a>
+    </li>
+    <li data-menuanchor="secondPage">
+      <div class="label"></div>
+      <a href="#secondPage">상품소개</a>
+    </li>
+    <li data-menuanchor="thirdPage">
+      <div class="label"></div>
+      <a href="#thirdPage">동영상</a>
+    </li>
+    <li data-menuanchor="fourthPage">
+      <div class="label"></div>
+      <a href="#fourthPage">검색</a>
+    </li>
+  </ul>
+</main>
+<?php
+get_footer();
